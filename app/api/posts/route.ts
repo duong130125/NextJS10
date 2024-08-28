0b3fdc906c557db0a1d1a016091820e5f782f81a
Duong <nguyendinhduong2005yt@gmail.com>
2024-08-28 08:11:20 +0700

    btss14_15

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
-  return NextResponse.json(data)
+  return NextResponse.json(data);
 }
